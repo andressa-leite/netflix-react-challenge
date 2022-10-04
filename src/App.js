f9ceee4fc0d15ce679fa75c74112dc8d71fedd94
andressa-leite <dre_tl@hotmail.com>
2022-10-04 20:31:03 -0300

    criado componente Row

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Nav from './Nav';
 import HomeScreen from './HomeScreen';
 import Banner from './Banner';
+import Row from './Row';
+import requests from './Request';
+
 
 function App() {
   return (
@@ -12,6 +15,13 @@ function App() {
 
       
       <Banner  />
+
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} /> 
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} /> 
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
