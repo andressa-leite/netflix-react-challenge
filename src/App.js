c5268aeb9da61403954a8f6cdab9a713917d0316
andressa-leite <dre_tl@hotmail.com>
2022-10-12 18:01:10 -0300

    implementados Login e Logout com redux

--- src/App.js
+++ src/App.js
@@ -4,15 +4,31 @@ import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './LoginScreen';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { auth } from './firebase';
+import { useSelector, useDispatch } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
 //it listens to any auth change, even if your logged in and you refresh it will store into your local memory
-  useEffect((
-    auth.onAuthStateChanged
-  ) =>{}, [])
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //Logged IN
+        dispatch(
+          login({
+          uid: userAuth.uid,
+          email: userAuth.email
+      }))
+      }else{
+        dispatch(logout)
+        //Logged OUT
+      }
+    });
+    return unsubscribe
+  }, []);
 
   return (
     <div className="App">
